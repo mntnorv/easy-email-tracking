f0bbfe16dd79593d8ee5ee85659591abb15c83e3
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-06 16:10:06 +0200

    Added 'details' and 'learn_more' pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
     render "index"
   end
+  
+  def learn_more
+    render "learn_more"
+  end
+  
+  def details
+    render "details"
+  end
 end
