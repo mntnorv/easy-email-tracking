27a506efe98691de2bfb60be032217d64c5188e8
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-16 18:41:40 +0200

    Added method to open modal from .hbs template

---
+++ app/assets/javascripts/modals.js
@@ -0,0 +1,10 @@
+$(document).bind('ready page:change', function() {
+	tooltips = $('[data-toggle="tooltip"]');
+	tooltips.tooltip('show');
+});
+
+function Modal(){};
+Modal.show = function (template, context) {
+	$('#modal-content').html(HandlebarsTemplates[template](context));
+	$('#modal').modal('show');
+};
