01f1d42ce04725c21fa5ed181f178bcd963db260
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-16 20:48:30 +0200

    Added tokenfield() on the recipients field

--- app/assets/javascripts/message-form.js
+++ app/assets/javascripts/message-form.js
@@ -1,6 +1,9 @@
 Modal.registerOpenHandler('message', function (content) {
 	form = content.find('form');
 	
+	recipientsField = form.find('#message_recipient_list');
+	recipientsField.tokenfield();
+	
 	form.submit(function (e) {
 		e.preventDefault();
 		console.log('It works!');
