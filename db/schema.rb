c7798f6b1a82945f4cd96b95ccee17f6ce1978e7
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-16 12:07:39 +0200

    Added recipient model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216083804) do
+ActiveRecord::Schema.define(version: 20140216094337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20140216083804) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "recipients", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "message_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "recipients", ["message_id"], name: "index_recipients_on_message_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
