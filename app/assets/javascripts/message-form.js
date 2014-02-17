db8b8c9dc91990bcd90048ee46c78f502669b443
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-17 20:33:14 +0200

    Added javascript email validation in the recipient field

--- app/assets/javascripts/message-form.js
+++ app/assets/javascripts/message-form.js
@@ -2,7 +2,36 @@ Modal.registerOpenHandler('message', function (content) {
 	form = content.find('form');
 	
 	recipientsField = form.find('#message_recipient_list');
-	recipientsField.tokenfield();
+
+	recipientsField.tokenfield().on('afterCreateToken', function(e) {
+		var re = /\S+@\S+\.\S+/;
+		var segments = e.token.value.split(/\s*[<>]/);
+		
+		var email = '';
+		if (segments.length > 1) {
+			email = segments[1];
+		} else {
+			email = segments[0];
+		}
+		
+		var valid = re.test(email);
+		
+		console.log('f', valid);
+		
+		if (segments.length > 3) {
+			valid = false;
+			console.log('if 1', valid);
+		} else if (segments.length == 3) {
+			valid = valid && /^\s*$/.test(segments[2]);
+			console.log('if 2', valid);
+		}
+		
+		console.log('fin', valid);
+		
+		if (!valid) {
+			$(e.relatedTarget).addClass('invalid');
+		}
+	});
 	
 	form.submit(function (e) {
 		e.preventDefault();
