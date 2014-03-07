7879ad936bca55c687c4811188e7eea2f9287205
Mantas Norvaiša <mntnorv@gmail.com>
2014-03-07 07:42:09 +0200

    Adding Google Analytics tracking image to every email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,15 @@
+require 'uri'
+
 class MessageMailer < ActionMailer::Base
   def message_email (message)
     @message = message
+    
+    tracking_id  = "UA-48184059-1"
+    client_id    = message.user.id
+    recipient_id = 1
+    
+    @tracking_url = "http://www.google-analytics.com/collect?v=1&tid=#{tracking_id}&cid=#{client_id}&t=event&ec=email&ea=open&el=#{recipient_id}&cs=newsletter&cm=email&cn=EasyTrack"
+    @tracking_url = URI.escape(@tracking_url)
     
     mail(
       to: message.recipients,
